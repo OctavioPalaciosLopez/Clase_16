d134b760defefd45f9280bede7d424a630444eb1
OctavioPalacios <ocpl2710@gmail.com>
2019-09-24 20:59:18 -0500

    Actividades clase 16

---
+++ server.js
@@ -0,0 +1,49 @@
+const express = require('express');
+const app = express()
+const port = 9000
+
+/********** EJERCICIO 1 ***********/
+/*app.get('/handler1', function (req, res) {
+    res.send('handler 1')
+})
+
+app.get('/handler2', function (req, res) {
+    res.send('handler 2')
+})*/
+
+
+/********** EJERCICIO 2 ***********/
+/*app.get('/handler1', function (req, res) {
+    res.send('Hola')
+    console.dir(req.baseUrl)
+    console.dir(req.body)
+    console.dir(req.hostname)
+    console.dir(req.method)
+    console.dir(req.originalUrl)
+    console.dir(req.path)
+    console.dir(req.protocol)
+    console.dir(req.query)
+})
+
+app.get('/handler2', function (req, res) {
+    res.send('handler 2')
+})*/
+
+/********** EJERCICIO 3 ***********/ 
+/*app.get('/handler1', function (req, res) {
+    res.status(200)
+    res.set('Content-Type', 'text/plain')
+    res.send('<h1>Hol</h1>')
+}) */
+
+app.use(function(req,res,next){
+    console.log('Soy middleware')
+    res.send('Soy middleware')
+    next()
+})
+app.listen(9000, () => { 
+    console.log(`Example app listening on port ${port}!`)
+})
+
+
+        
